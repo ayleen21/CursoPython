d1d5bcc17ff673f3659e48222c65b4baff3f3acc
ayleen21 <ayleenmurillo77@gmail.com>
2022-01-03 16:30:47 -0500

    Avances del curso hasta el video 41

---
+++ Modulos.py/uso_funciones.py
@@ -0,0 +1,8 @@
+#import funcionesMatematicas #importar otro archivo
+
+from funcionesMatematicas import * #Para tener disponible todo lo que tenemos en el modulo
+sumar(7,5)
+
+restar(9,5)
+
+multiplicar(8,7)
